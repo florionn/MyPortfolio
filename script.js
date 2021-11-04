e9de8c04f7852d914549ca87b4f015967f2a2df1
florionn <89059232+florionn@users.noreply.github.com>
2021-11-04 12:29:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-setInterval(myFunction, 1000);
+const lang = document.getElementById("lang"),
+      bio = document.getElementById("bio"),
+      englishBio = `My name is Florion Nako. 
+      <br> I am a Computer and Mobile Repair Specialist with over five years of experience in the field.
+      <br> Currently working for Geek Squad, I strive to deliver the best technical solution for the needs of the costumer. 
+      <br> In my free time, I enjoy playing sports and occasionally PC gaming. 
+      <br> Since August 2021 I have been studying Web Development through a certificate program at MIT.`;
+      spanishBio = `Mi nombre es Florion Nako.
+      <br>Soy especialista en reparación de computadoras y móviles con más de cinco años de experiencia en el campo.
+      <br>Actualmente trabajando para Geek Squad, me esfuerzo por ofrecer la mejor solución técnica para las necesidades del cliente.
+      <br>En mi tiempo libre, disfruto jugando deportes y ocasionalmente juegos de PC.
+      <br>Desde agosto de 2021 he estado estudiando Programacion de Paginas de Internet a través de un programa de certificación en el MIT.`;
+var langState = "English";
 
-function myFunction() {
-  let d = new Date();
-  document.getElementById("div1").innerHTML=
-  "Current Time : "+
-  d.getHours() + ":" +
-  d.getMinutes() + ":" +
-  d.getSeconds();
+function myFunc(){
+  if (langState==="English"){
+    bio.innerHTML=spanishBio;
+    lang.innerHTML="English";
+    langState="Spanish";
+  }
+  else{
+    bio.innerHTML=englishBio;
+    lang.innerHTML="Spanish";
+    langState="English";
+  }
 }
